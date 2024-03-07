8a1093dd334daeb04c34e5c5f240c357b61a328e
Joe Watt <joewatt95@u.nus.edu>
2024-03-07 22:46:20 +0800

    bump

--- src/l4_lp/swipl/js/wasm_query.cljs
+++ src/l4_lp/swipl/js/wasm_query.cljs
@@ -6,7 +6,7 @@
             [shadow.esm :refer [dynamic-import]]))
 
 (def ^:private swipl-wasm-cdn-url
-  "https://SWI-Prolog.github.io/npm-swipl-wasm/3/7/1/dynamic-import.js")
+  "https://SWI-Prolog.github.io/npm-swipl-wasm/3/7/3/dynamic-import.js")
 
 (def ^:private prelude-qlf-url
   "resources/swipl/prelude.qlf")
