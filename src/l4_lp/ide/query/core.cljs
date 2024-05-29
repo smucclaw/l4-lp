920dec173eeeebe2af54dc3fa3bf7403bb556c5d
joewatt95 <joewatt95@u.nus.edu>
2024-05-29 13:10:20 +0800

    add a note and default args

--- src/l4_lp/ide/query/core.cljs
+++ src/l4_lp/ide/query/core.cljs
@@ -12,13 +12,25 @@
   (new js/Worker "/js/l4_ide/query_worker.js"
        #js {:type "module"}))
 
+;; This tells the web worker to load the swipl prelude qlf file from a URL
+;; relative to that of the parent web app.
+;; This is needed as the web worker does not know of the parent web app's URL,
+;; and does not have access to its js/window object.
+;; The only way to communicate this info from the parent app to the worker is
+;; to post it over as a message, via the web worker API.
 (post-data-as-js! :worker worker
                   :tag "swipl-prelude-qlf-url"
                   :payload swipl-prelude-qlf-url)
 
+(def ^:private no-op
+  (constantly nil))
+
 (defn transpile-and-query!
   [l4-program
-   & {:keys [on-transpiled-prolog on-query-result on-done]}]
+   & {:keys [on-transpiled-prolog on-query-result on-done]
+      :or {on-transpiled-prolog no-op
+           on-query-result no-op
+           on-done no-op}}]
   (let [on-message!
         (jsi/fn [^:js {:keys [data]}]
           (m/match data
