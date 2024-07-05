8aab15e594811ff39059a374688d69debc434d56
Joe Watt <joewatt95@u.nus.edu>
2024-07-05 10:20:14 +0800

    t

--- install_deps.pl
+++ install_deps.pl
@@ -1,4 +1,5 @@
-Opts = [interactive(false), test(false)],
-pack_install(date_time, [version('0.1.4') | Opts]),
-pack_install(clpBNR, [version('0.11.8') | Opts]),
-halt.
+install_deps :-
+  Opts = [interactive(false), test(false)],
+  pack_install(date_time, [version('0.1.4') | Opts]),
+  pack_install(clpBNR, [version('0.11.8') | Opts]),
+  halt.
